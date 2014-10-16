c51e6bd69a4572d9f41217e17950a2be1839f091
George Rentis <georent123@hotmail.com>
2014-10-16 16:22:07 +0300

    Calculator 3rd version

--- calculator.js
+++ calculator.js
@@ -1,48 +1,61 @@
 var first=0;
 var second=0;
+var third=0;
 var total=0;
 var msg="";
 var txt1="";
 var eq="";
+var i=0;
+var total2=0;
+var k=0;
 $(document).ready(function(){
 
 $(".unlimited").on("click","button",function(){
     first=+$(this).val();
-    $(".txt").val(first);
+    if (i>=1){
+$(".txt").val(eq+first);
+    eq=$(".txt").val();
+    ;}
 
-if(msg=="+"){
-    total=second+first;
+    if (i==0){
+$(".txt").val(first);
+    eq=$(".txt").val();
 
-}
-if(msg=="-"){
-    total=second-first;
+    }
 
-}
-if(msg=="*"){
-    total=second*first;
-
-}
-if(msg=="/"){
-    total=second/first;
-
-}
+i=i+1;
 
 
 
-
-
-    second=first;
-
+    second=+eq;
 
 });
 
 
 $(".action").on("click","button",function(){
 
-
-
+if (k==0){
+third=second;
+k=1;
+}
 msg=$(this).val();
 if(msg=="="){
+  if(txt1=="+"){
+    total=third+second;
+
+}
+if(txt1=="-"){
+    total=third-second;
+
+}
+if(txt1=="*"){
+    total=third*second;
+
+}
+if(txt1=="/"){
+    total=third/second;
+
+}
   $(".txt").val(total);
       if (total!=0 ){
       second=total;
@@ -50,13 +63,18 @@ if(msg=="="){
 }
 }
 
- if (msg=="clr"){
-$(".txt").val("");
-first=0;
-second=0;
-total=0;
- }
-
+               if (msg=="clr"){
+              $(".txt").val("");
+              first=0;
+              second=0;
+              total=0;
+              third=0;
+              eq="";
+              k=0;
+               }
+i=0;
+if(msg!="=")
+{txt1=msg;}
 
     });
 
